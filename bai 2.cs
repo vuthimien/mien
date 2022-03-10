953f23f01b3dcb4552dbee8473f6f8a25d119c71
vuthimien <77420664+vuthimien@users.noreply.github.com>
2022-03-10 11:05:02 +0700

    Add files via upload

---
+++ bai 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float s = 0;
+            int count = 1;
+            Console.WriteLine("nhap n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {             
+                    s = s + 1 / (float)i;
+                count++;
+            }
+            Console.WriteLine("tong la:"+s);
+        }
+    }
+}
---
+++ bai 3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai3.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mang = new int[100];
+            int so, count = 0;
+            Console.Write("Nhap kich co mang:");
+            so = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < so; i++)
+            {
+                Console.WriteLine("Nhap so thu {0}:", i + 1);
+                mang[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < so; i++)
+            {
+                if (mang[i] % 2 == 1)
+                    Console.WriteLine("so le:" + mang[i]);
+                count++;
+            }
+            if (count == 0)
+                Console.WriteLine("Không có số lẻ");
+
+        }
+    }
+}
